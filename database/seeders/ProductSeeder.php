51ed1537f86310e0e5db86207ad5078d3f6293cf
tgldr1014 <toogoo0410@gmail.com>
2026-05-12 09:45:43 +0800

    database seeder зассан

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -24,14 +24,12 @@ class ProductSeeder extends Seeder
             ['name' => 'Хосын багц', 'category' => 'bagts', 'price' => 55000, 'image' => 'images/menu/couple_set.svg'],
             ['name' => 'Найзууд багц', 'category' => 'bagts', 'price' => 85000, 'image' => 'images/menu/friends_set.svg'],
             ['name' => 'Гэр бүлийн багц', 'category' => 'bagts', 'price' => 95000, 'image' => 'images/menu/family_set.svg'],
-            ['name' => 'Кимчи Порк Багц', 'category' => 'bagts', 'price' => 55000, 'image' => 'https://via.placeholder.com/300x150', 'description' => 'Кимчи, гахайн махтай багц'],
 
             ['name' => 'Пепперони Пицца', 'category' => 'pizza', 'price' => 28000, 'image' => 'images/menu/pepperoni.svg'],
             ['name' => 'Маргарита Пицца', 'category' => 'pizza', 'price' => 24000, 'image' => 'images/menu/margherita.svg'],
             ['name' => 'Махны цуглуулга', 'category' => 'pizza', 'price' => 32000, 'image' => 'images/menu/meat_lovers.svg'],
             ['name' => 'Хавайн Пицца', 'category' => 'pizza', 'price' => 29500, 'image' => 'images/menu/hawaiian.svg'],
             ['name' => 'Веган Пицца', 'category' => 'pizza', 'price' => 26000, 'image' => 'images/menu/vegan.svg'],
-            ['name' => 'Мах сонирхогчдын пицца', 'category' => 'pizza', 'price' => 25000, 'image' => 'https://via.placeholder.com/300x150', 'description' => 'Махтай пицца'],
 
             ['name' => 'Сонгодог Бургер', 'category' => 'burger', 'price' => 12500, 'image' => 'images/menu/standart_burger.svg'],
             ['name' => 'Чизбургер', 'category' => 'burger', 'price' => 13500, 'image' => 'images/menu/double_cheese.svg'],
@@ -44,7 +42,6 @@ class ProductSeeder extends Seeder
             ['name' => 'Orange Juice', 'category' => 'undaa', 'price' => 3500, 'image' => 'images/menu/lemonade.svg'],
             ['name' => 'Lipton Iced Tea', 'category' => 'undaa', 'price' => 3000, 'image' => 'images/menu/icedtea.svg'],
             ['name' => 'Цэвэр ус 0.5L', 'category' => 'undaa', 'price' => 1500, 'image' => 'images/menu/water.svg'],
-            ['name' => 'Pepsi 1.5L', 'category' => 'undaa', 'price' => 4500, 'image' => 'https://via.placeholder.com/300x150', 'description' => 'Содан ундаа'],
         ];
 
         foreach ($products as $product) {
